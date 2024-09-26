8bc96092c0239b0fa0e8ba0b84e63ba1e4e163e4
Prodyumna Borgohain <128952878+ArnoVivien@users.noreply.github.com>
2024-09-26 18:55:52 +0530

    Assignment : 1 (25/09/24)

--- interchange_row_col.c
+++ interchange_row_col.c
@@ -46,7 +46,7 @@ main() {
 
     interchange(matrix, size);
 
-    printf("Interchanged matrix:\n");
+    printf("Transposed matrix:\n");
     for (i = 0; i < size; i++) {
         for (j = 0; j < size; j++) {
             printf("%d ", matrix[i][j]);
